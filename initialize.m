f62713f3ee46fbca39c6ff8fb2530b0afb97893b
Alen <alen.kushova@libero.it>
2021-05-22 18:36:38 +0200

    Add files via upload
    File that adds the path of required folders:
    means also we need GeoPDEs periodic

---
+++ initialize.m
@@ -0,0 +1,13 @@
+% A script to add paths.
+clear;
+
+currentpath = pwd;
+
+addpath (genpath ('geopdes-periodic'))
+addpath (genpath ('geopdes'))
+addpath (genpath ('nurbs'))
+addpath (genpath ('geopdes_hierarchical'))
+addpath (genpath ('data'))
+addpath (genpath ('quasi_interpolants'))
+addpath (genpath ('solvers'))
+
